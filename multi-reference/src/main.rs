3a02b7c1226ec386ff5bfd3058b1b866830587c3
Mickey <hedgefund996@gmail.com>
2022-05-16 20:27:28 +0800

    multi reference && dangling pointer
    1.reference的lifespan是从declare到最终使用
    
    2.同一scope，要么只能有一个&mut reference，或者同时有多个reference；不存在reference不被release还能&mut reference的情况
    
    3.rust非常严格，不存在dangling pointer的情况，如果内存被释放，pointer也会被释放

---
+++ multi-reference/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let mut s = String::from("ok");
+
+    let mut s1 = &mut s;
+    // rust中一个value不能同时被两个&mut reference，可以理解为value在任何时刻尤其仅有一个owner
+    // 但如果超出了ownership，就没什么问题了（因为之前的reference将被释放）
+    let s2 = &mut s;
+    // 对&mut reference再进行一次reference是可以的
+    let s2 = &mut s1;
+}
+
