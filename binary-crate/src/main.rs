2717d6de7250969a555052aa7f35013c5d0066e1
Mickey <hedgefund996@gmail.com>
2022-06-10 02:07:55 +0800

    pub struct
    1）pub struct，不代表默认pub所有字段
    
    2）需要一个关联函数，pub实例

--- binary-crate/src/main.rs
+++ binary-crate/src/main.rs
@@ -1,3 +1,28 @@
 fn main() {
-    println!("Hello, world!");
+    mod back_of_house {
+        pub struct Breakfast {
+            pub toast: String,
+            seasonal_fruit: String,
+        }
+    
+        impl Breakfast {
+            // 返回一个public的实例
+            pub fn summer(toast: &str) -> Breakfast {
+                Breakfast {
+                    toast: String::from(toast),
+                    seasonal_fruit: String::from("peaches"),
+                }
+            }
+        }
+    }
+    
+    pub fn eat_at_restaurant() {
+        // 这里只能调用summer实例（一个关联函数）
+        // 因为有private的字段
+        let mut meal = back_of_house::Breakfast::summer("Rye");
+        meal.toast = String::from("Wheat");
+        println!("I'd like {} toast please", meal.toast);
+    
+        // meal.seasonal_fruit = String::from("blueberries");
+    }
 }
