fd284350cbd4ca9c2f021554401d13e8e0d3a05f
Mickey <hedgefund996@gmail.com>
2022-07-04 13:08:17 +0800

    thread & scope issue

--- multi-thread/src/main.rs
+++ multi-thread/src/main.rs
@@ -1,16 +1,22 @@
-use std::thread;
 use std::time::Duration;
 
 fn main() {
 
  let mut num = Vec::with_capacity(11);
 
- std::thread::spawn(move || {
-     for i in 0..10 {
-        num.push(i);
-     }
- });
+ {
+    std::thread::spawn(move || {
+        for i in 0..10 {
+           num.push(i);
+        }
+    });
+ }
+
 
 //  &num.push(11);
- println!("{}", num.capacity());
+ {
+    println!("{}", num.capacity());
+ }
 }
+
+// fix thread issue
